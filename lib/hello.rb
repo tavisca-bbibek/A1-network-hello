db1ed5236f70a8fc1528fe2be8efd3008856c29c
Bibek BC <bbibek@tavisca.in>
2019-06-19 20:36:06 +0545

    Hello user Greeter

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,4 +1,7 @@
+require 'greeter'
+
 # Default is "World"
-# Author Bibek BC (bbibek@tavisca.in)
 name = ARGV.first || "World"
-puts "Hello, #{name}!"
+
+greeter = Greeter.new(name)
+puts greeter.greet
